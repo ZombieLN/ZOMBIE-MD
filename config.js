38b1fde6045aa47e8f64e75d8557660691c217f1
ZombieLN <dilishagim@gmail.com>
2025-11-16 19:04:35 +0530

    Z O M B I E . I N J E C T . . . .

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2JlwmATD#zLegNqDof9o5a9R08pD-_eaz35j3zTtGQaV_BNEuhOY",
-MONGODB: process.env.MONGODB || "mongodb://mongo:MgRxoQTXWnKFtprOrocHAeucSEKWBtoE@junction.proxy.rlwy.net:45585",
+SESSION_ID: process.env.SESSION_ID || "HIcX3QZS#ga-fQ6AI56L9eMiazrk-Y9lOn26KQjFDjdaL1u9e738",
+MONGODB: process.env.MONGODB || "mongodb://mongo:LIgXtSmvDMcshanQicXNZTLQpnstpplG@switchyard.proxy.rlwy.net:12462",
 };
